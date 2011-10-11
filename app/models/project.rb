9a2e3b10787d278435cb5ec3629d3f60be2198d3
Rafael Martins <rafael.mws@gmail.com>
2011-10-10 21:24:41 -0300

    ordering users of project per name

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,8 +1,8 @@
 class Project < ActiveRecord::Base
-  has_and_belongs_to_many :users
+  has_and_belongs_to_many :users, :order => 'name ASC'
   has_many :tasks, :dependent => :destroy
 
   validates :name,  :presence => true
   validates_length_of :users, :minimum => 1
-	
+
 end
